d19d2c953fe16a990d646d8116fee7e2e66cbc75
Amber Saxena <ambersaxena0925@gmail.com>
2025-10-30 15:08:59 +0530

    Revert "."
    This reverts commit 6a7fee782ab0cd8828fad4cdb697afc98611de23.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,10 +10,4 @@ createRoot(document.getElementById('root')!).render(
       <App />
     </AuthProvider>
   </StrictMode>
-);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
 );
